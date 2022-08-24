9e912ff1a18efacbd9c3f5d09e2a630c64b9be0e
100rab100 <patil.100rabh@gmail.com>
2022-08-24 14:09:58 +0530

    one way

--- kth largest number (2).py
+++ kth largest number (2).py
@@ -1,18 +1,18 @@
 
-def getPairsCount(arr, n, sum):
+num = 2
+# define a flag variable
+flag = False
 
-	count = 0 # Initialize result
+if num > 1:
+    # check for factors
+    for i in range(2, num//2+1):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
 
-	for i in range(0, n):
-		for j in range(i + 1, n):
-			if arr[i] + arr[j] == sum:
-				count += 1
-
-	return count
-
-
-# Driver function
-arr = [1, 5, 7, -1, 5]
-n = len(arr)
-sum = 6
-print("Count of pairs is",getPairsCount(arr, n, sum))
+if flag:
+    print(num, "is not a prime number")
+else:
+    print(num, "is a prime number")
