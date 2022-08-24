9ef2492e36105d11d9848f786772f0d0a0f99f0a
100rab100 <patil.100rabh@gmail.com>
2022-08-24 14:20:05 +0530

    one way

--- kth largest number (2).py
+++ kth largest number (2).py
@@ -1,27 +1,14 @@
 
-def areRotations(string1, string2):
-	size1 = len(string1)
-	size2 = len(string2)
-	temp = ''
+def linearSearch(arr, n):
+	for i in range(n):
+		if arr[i] is i:
+			return i
+	# If no fixed point present then return -1
+	return -1
 
-	# Check if sizes of two strings are same
-	if size1 != size2:
-		return 0
+# Driver program to check above functions
+arr = [-10, -1, 0, 3, 10, 11, 30, 50, 100]
+n = len(arr)
+print("Fixed Point is " + str(linearSearch(arr, n)))
 
-	# Create a temp string with value str1.str1
-	temp = string1 + string1
-
-	if (temp.count(string2)> 0):
-		return 1
-	else:
-		return 0
-
-# Driver program to test the above function
-string1 = "AACD"
-string2 = "ACDA"
-
-if areRotations(string1, string2):
-	print ("Strings are rotations of each other")
-else:
-	print ("Strings are not rotations of each other")
 
