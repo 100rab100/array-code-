962f23f7b3468a3daaaf82ce4acea1f85b430dfd
100rab100 <patil.100rabh@gmail.com>
2022-08-25 16:58:18 +0530

    one way

--- kth largest number (2).py
+++ kth largest number (2).py
@@ -1,15 +1,3 @@
-def bsdk(nums, target):
-    for i in range(n):
-
-        for j in range(i + 1, n):
-           if target == nums[i] + nums[j]:
-               return ([i,j])
-              # break
-
-          #  return [i,j]
-
-
-nums = [2,7,11,15]
-n = len(nums)
-target = 9
-print(bsdk(nums, target))
+nums=[1,2,3,4,4]
+nums.sort()
+print(nums)
