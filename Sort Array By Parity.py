4473b8a6554414cd451454a7c811dcdcb781a864
100rab100 <99138605+100rab100@users.noreply.github.com>
2022-08-28 13:31:53 +0530

    Update Sort Array By Parity.py

--- Sort Array By Parity.py
+++ Sort Array By Parity.py
@@ -1,3 +1,14 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:  
         return [i for i in nums if i % 2 ==0]+[i for i in nums if i % 2 != 0]
+    
+    
+    
+    #2
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:  
+        if len(nums) == 1: return nums
+        l = []
+        for i in nums:
+            l.insert(0,i) if i % 2 == 0 else l.append(i)
+        return l  
