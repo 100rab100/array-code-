12f3d84eec18b16916f3c35bf105f140e514f442
100rab100 <patil.100rabh@gmail.com>
2022-08-29 00:44:36 +0530

    one way

--- The kth Factor of n.py
+++ The kth Factor of n.py
@@ -1,4 +1,27 @@
-s= "Hello,welcome to my world."
-i="to"
+# Python3 program to print
+# all primes less than N
 
-print(s.find(i) for i in s)
+# Function to check whether
+# a number is prime or not .
+def isPrime(n):
+    # Corner case
+    if n <= 1:
+        return False
+
+    # check from 2 to n-1
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+
+    return True
+
+
+# Function to print primes
+def printPrime(n):
+    for i in range(2, n + 1):
+        if isPrime(i):
+            print(i, end=" ")
+
+n=89
+printPrime(n)
+
