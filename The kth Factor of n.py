dfbe72e1ccf46a1b6849f84479d3a1bae60c008e
100rab100 <patil.100rabh@gmail.com>
2022-08-28 13:25:08 +0530

    one way

--- The kth Factor of n.py
+++ The kth Factor of n.py
@@ -1,9 +1,4 @@
-def subsets( nums):
-    res = [[]]
-    for n in nums:
-        for i in range(len(res)):
-            res.append(res[i] + [n])
-            print(res)
-    return res
-nums=[1,2]
-print(subsets(nums))
+s= "Hello,welcome to my world."
+i="to"
+
+print(s.find(i) for i in s)
