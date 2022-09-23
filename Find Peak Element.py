92f2d0f381f9a6442d3fc98ba6f8f988652b1579
100rab100 <99138605+100rab100@users.noreply.github.com>
2022-09-23 13:52:52 +0530

    Update Find Peak Element.py

--- Find Peak Element.py
+++ Find Peak Element.py
@@ -2,3 +2,9 @@ class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         return nums.index(max(nums))
             
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        for i in range(len(nums)-1):
+            if nums[i]>nums[i+1]:
+                return i
+        return len(nums)-1
