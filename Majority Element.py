d095e88e03a357470940f3887d59864f9a8a6c32
100rab100 <99138605+100rab100@users.noreply.github.com>
2022-09-23 14:45:24 +0530

    Update Majority Element.py

--- Majority Element.py
+++ Majority Element.py
@@ -2,7 +2,13 @@ class Solution:
     def majorityElement(self, num: List[int]) -> int:
         return sorted(num)[int(len(num)/2)]
        
-        
+ class Solution:
+    def majorityElement(self, num: List[int]) -> int:
+        m = {}
+        for n in num:
+            m[n] = m.get(n, 0) + 1
+            if m[n] > len(num)//2:
+                return n     
        
     
         
